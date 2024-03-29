fdaa2aeddef7186c77a07072ff328b298a07085b
Prasit Sutthikamolsakul <51251028+prasitstk@users.noreply.github.com>
2024-03-29 09:45:12 +0000

    No need to include random string into the DynamoDB table name.

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "aws_s3_bucket_public_access_block" "backend_bucket_public_access_block
 #######################
 
 resource "aws_dynamodb_table" "backend_state_lock_tbl" {
-  name = "terraform-backend-state-lock-<random-string>"
+  name = "terraform-backend-state-lock"
 
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
